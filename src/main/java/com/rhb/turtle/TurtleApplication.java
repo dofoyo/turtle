a367bc0868df1f4648cd102cfa00bed016855ae0
dofoyo <dofoyo@sina.cn>
2019-02-02 14:23:16 +0800

    Initial commit

---
+++ src/main/java/com/rhb/turtle/TurtleApplication.java
@@ -0,0 +1,14 @@
+package com.rhb.turtle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtleApplication.class, args);
+	}
+
+}
+
